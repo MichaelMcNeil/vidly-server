67bb8e272b0af8561a875419ad23363c32efff3d
Michael McNeil <me@michaelmcneil.net>
2019-09-22 10:48:21 -0400

    Added support to disable auth in config and default jwtKey

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("./startup/validation")();
 
 if (process.env.NODE_ENV === "production") require("./startup/prod")(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,9 @@
+const config = require("config");
 
 module.exports = function(req, res, next) {
-    if(!req.user.isAdmin) return res.status(403).send('Access Denied.');
+  if (!config.get("requiresAuth")) return next();
 
-    next();
-}
+  if (!req.user.isAdmin) return res.status(403).send("Access Denied.");
+
+  next();
+};
